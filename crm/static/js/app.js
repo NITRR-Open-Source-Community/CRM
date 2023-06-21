f49a5ce43ba33c34d527fe3f24fb16c276319b2f
Pratyush Nagpal <h83pratyush@gmail.com>
2023-06-21 17:06:06 +0530

    First Commit

---
+++ crm/static/js/app.js
@@ -0,0 +1,20 @@
+
+// Message/Notification timer
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+
+{
+
+    message_timeout.style.display = "none";
+
+
+}, 5000);
+
+
+
+
+
+
+
